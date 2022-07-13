1c81afe9230cd18fceb491232f95297f8b0984e6
pvborgesz <pvbo.lima@discente.ufma.br>
2022-07-12 22:49:28 -0300

    Implementação do pomodoro

---
+++ src/components/PomodoroTimer.tsx
@@ -0,0 +1,79 @@
+import {useEffect, useState} from 'react'
+import {useInterval } from '../hooks/useInterval'
+import {secondsToTime} from '../utils/secondsToTime'
+import {Button} from '../components/Button'
+import { Timer } from './Timer'
+
+interface Props{
+    pomodoroTime:number
+    longRestTime:number
+    shortRestTime:number
+    cycles:number
+    status: string
+}
+
+export function PomodoroTimer(props:Props) : JSX.Element{
+    const [mainTime, setMainTime] = useState(props.pomodoroTime)
+    const [timeCounting, setTimeCounting] = useState(false)
+    const [working, setWorking] = useState(false)
+    const [status, setStatus] = useState('Descansando')
+
+    useEffect( () =>{
+        if (working) {
+            document.body.classList.add('working')
+            setStatus('Trabalhando')
+        } else {
+            document.body.classList.remove('working')
+        }
+    }, [working])
+
+    useInterval(()=>{
+        setMainTime(mainTime-1)
+    }, timeCounting ? 1000 : null)
+
+    const configureWork: any = () => {
+        setStatus('Trabalhando')
+        setTimeCounting(true)
+        setWorking(true)
+    }
+
+    const restartWork: any = () =>{
+        setStatus('Descansando')
+        setMainTime(props.pomodoroTime)
+        setTimeCounting(!timeCounting)
+        setWorking(false)
+    }
+
+    const pauseWork: any = () => {
+        setWorking(false)
+        setTimeCounting(!timeCounting)
+        setMainTime(props.shortRestTime)
+        setTimeCounting(true)
+        setStatus('Descansando')
+    }
+
+    const longPause: any = () =>{
+        setWorking(false)
+        setTimeCounting(!timeCounting)
+        setMainTime(props.longRestTime)
+        setTimeCounting(true)
+        setStatus('Descansando')
+    }
+    return (
+        <div className="pomodoro">
+            <h2>Você está: {status}</h2> 
+            <Timer mainTime={mainTime}></Timer>
+            
+            <div className="controls">
+                
+                {(working) ? <Button onClick={ () => pauseWork()} text={'Pause'}></Button> : <Button onClick={ () => configureWork()} text={'Start'}></Button>}
+                {/* <Button onClick={ () => longPause()} text="Pausa Longa"></Button> */}
+                <Button onClick={ () => restartWork()} text="Reiniciar"></Button>
+            </div>
+
+            <div className="details">
+                {/* <p> Testando: </p> */}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Timer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { secondsToTime } from '../utils/secondsToTime'
+
+interface Props{
+    mainTime: number
+}
+
+export function Timer(props : Props) : JSX.Element{
+    return (
+        <div className="timer">
+            {secondsToTime(props.mainTime)}
+        </div>
+    )
+} 
