6cca69114928516ef2a6e0ff5a097e7825e28839
pvborgesz <pvbo.lima@discente.ufma.br>
2022-07-12 23:07:39 -0300

    Adição do footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,16 @@ function App() : JSX.Element {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="container">
-      <PomodoroTimer pomodoroTime={1500} longRestTime={900} shortRestTime={300} cycles={4} status={'Descansando'}/>
-    </div>
+    <>
+      <div className="container">
+        <PomodoroTimer pomodoroTime={1500} longRestTime={900} shortRestTime={300} cycles={4} status={'Descansando'}/>
+      </div>
+      <div className="footer">
+            <p>
+                <a href="https://github.com/pvborgesz">Desenvolvido por Paulo Victor Borges</a>
+            </p>
+      </div>
+    </>
   )
 }
 
